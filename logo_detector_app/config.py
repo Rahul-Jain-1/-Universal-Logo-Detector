3f1947ceb60a8605dfb9f63a619146c7f5d40456
Rahul-Jain-1 <jainshabrahul@gmail.com>
2025-05-28 12:10:35 +0530

    update model and genai code

--- logo_detector_app/config.py
+++ logo_detector_app/config.py
@@ -9,7 +9,7 @@ LAYOUT = "centered"
 # model dir and gemini key
 MODEL_DIR = os.getenv("MODEL_DIR","src/model")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MODEL_NAME = " gemini-2.0-flash"
+MODEL_NAME = "gemini-2.0-flash"
 
 # Object detection threshold
 DETECTION_THRESHOLD = 0.5
